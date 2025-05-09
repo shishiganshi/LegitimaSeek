27083d99e0f29753b3122a42c6336b036d8e40fa
shishiganshi <esijas.cyruz@gmail.com>
2025-05-10 01:06:40 +0800

    Update loading-screen.js

--- js/loading-screen.js
+++ js/loading-screen.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
-        window.location.href = './main.html';
+        window.location.href = './app.html';  // Direct path to app.html, no session storage needed
     }, 7000);
 });
