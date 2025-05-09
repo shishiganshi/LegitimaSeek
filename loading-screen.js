64154362d014142681d817110399b820e2aae5c2
shishiganshi <esijas.cyruz@gmail.com>
2025-05-10 00:46:07 +0800

    Delete loading-screen.js

--- loading-screen.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const mainPage = sessionStorage.getItem('mainPage') || window.location.href.replace('loading-screen.html', 'main.html');
-    sessionStorage.setItem('mainPage', mainPage);
-
-    setTimeout(() => {
-        window.location.href = mainPage;
-    }, 7000);
-});
